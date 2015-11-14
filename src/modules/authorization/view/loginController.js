1f9584336181c065f1d4b53880b03af4be415d24
Krzysiek <krzysztof.wolinski@cohesiva.com>
2015-11-14 13:44:29 +0100

    Finished working on login page and controller

--- src/modules/authorization/view/loginController.js
+++ src/modules/authorization/view/loginController.js
@@ -4,21 +4,37 @@ angular.module('medicalDiagnosis.authorization')
 	.controller('loginController', [
 		'$scope',
 		'AuthService',
-		function($scope, AuthService) {
+		'AUTH_RESPONSE_STATUS',
+		function($scope, AuthService, AUTH_RESPONSE_STATUS) {
 			
-			$scope.login = AuthService.login;
+			$scope.loginFailed = false;
 			$scope.user = {
 				username: '',
 				password: ''
 			};
 
-			$scope.isSubmitButtonEnabled = function() {
+			$scope.isSubmitEnabled = function() {
 				if (($scope.user.username !== '') && ($scope.user.password != '')) {
 					return true;
 				} else {
 					return false;
 				}
 
+			}
+
+			$scope.login = function() {
+				if ($scope.isSubmitEnabled()) {
+					AuthService.login($scope.user.username, $scope.user.password).then(function(result) {
+						if (result === AUTH_RESPONSE_STATUS.ok) {
+							$scope.loginFailed = false;	
+						} else {
+							$scope.loginFailed = true;	
+						}
+						
+					}, function() {
+						$scope.loginFailed = true;
+					});
+				}
 			}
 
 		}
